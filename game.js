3e279f1c2c6708964e60e86354171f2dcf14b52d
Daniel Gerdes <daniel.gerdes.hagen@outlook.de>
2018-05-22 22:00:44 +0200

    Init

---
+++ game.js
@@ -0,0 +1,26 @@
+var game = new Phaser.Game(1024, 600, Phaser.AUTO, 'test', { preload:preload, create: create, update: update, render: render});
+
+function preload() {
+
+    game.load.image('hoch','Assets/Wuerfeloben.png');
+    game.load.image('runter','Assets/Wuerfelunten.png');
+    game.load.image('links','Assets/Wuerfellinks.png');
+    game.load.image('rechts','Assets/Wuerfelrechts.png');
+    game.load.image('background','Assets/Hintergrund.png');
+    game.load.image('house','Assets/housetest.png');
+
+}
+
+
+
+function create () {
+
+} 
+
+function update() {
+    
+}
+
+function render () {
+    
+}
